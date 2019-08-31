59ddf4378e8ba19e2fd599e40c2522393c2303ab
jvl <jli141928@gmail.com>
2019-08-31 14:55:28 -0700

    Add functionality to build ActorObject csv

--- emby_thumbs/Get-EmbyActorThumbs.ps1
+++ emby_thumbs/Get-EmbyActorThumbs.ps1
@@ -34,79 +34,50 @@ function New-ActorObject {
 # Remove progress bar to speed up REST requests
 $ProgressPreference = 'SilentlyContinue'
 
-$SettingsPath = Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath (Join-Path -Path '..' -ChildPath 'settings_sort_jav.ini'))
 # Check settings file for config
+$SettingsPath = Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath (Join-Path -Path '..' -ChildPath 'settings_sort_jav.ini'))
 $EmbyServerUri = ((Get-Content $SettingsPath) -match '^emby-server-uri').Split('=')[1]
 $EmbyApiKey = ((Get-Content $SettingsPath) -match '^emby-api-key').Split('=')[1]
-$R18File = ((Get-Content $SettingsPath) -match '^r18-export-csv-path').Split('=')[1]
-
-
-
-# Create db directory in script path
-#New-Item -ItemType Directory -Path (Join-Path -Path $PSScriptRoot -ChildPath "db") -ErrorAction SilentlyContinue
+$R18ImportPath = ((Get-Content $SettingsPath) -match '^r18-export-csv-path').Split('=')[1]
+$ActorExportPath = ((Get-Content $SettingsPath) -match '^actor-csv-export-path').Split('=')[1]
 
 # Write Emby actors and id to object
 $EmbyActors = Get-EmbyActors -ServerUri $EmbyServerUri -ApiKey $EmbyApiKey
-$EmbyObject = @()
+$EmbyActorObject = @()
 for ($x = 0; $x -lt $EmbyActors.Items.Length; $x++) {
-    $EmbyObject += New-Object -TypeName psobject -Property @{
+    $EmbyActorObject += New-Object -TypeName psobject -Property @{
         Name = $EmbyActors.Items.Name[$x]
         EmbyId = $EmbyActors.Items.Id[$x]
     }
 }
 
-# Write R18 actors and thumbs to object
-$R18Csv = Import-Csv -Path $R18File
-$R18Object = @()
-$Temp = @()
-foreach ($Object in $R18Csv) {
-    # Write names to temp array
-    $Temp += $Object.alt
-    # If name already exists in array, don't write another. Testing to sort as usually the first entry is the most popular actress
-    if ($Object.alt -notmatch $Temp) {
-        $R18Object += New-Object -TypeName psobject -Property @{
-            Name = $Object.alt
-            ThumbUrl = $Object.src
+# Import R18 actors and thumburls to object
+$R18ActorObject = Import-Csv -Path $R18ImportPath
+
+# Compare both Emby and R18 actors for matching actors, and combine to a single object
+$ActorNames = @()
+$ActorObject = @()
+for ($x = 0; $x -lt $EmbyActorObject.Length; $x++) {
+    $ActorNames += ($EmbyActorObject[$x].Name).ToLower()
+    if ($ActorNames[$x] -notin $R18ActorObject.Name) {
+        #Write-Host "Missing"
+        $ActorObject += New-Object -TypeName psobject -Property @{
+            Name = $EmbyActorObject[$x].Name
+            EmbyId = $EmbyActorObject[$x].EmbyId
+            ThumbUrl = ''
+            PrimaryUrl = ''
+        }
     }
-}
-
-
-
-#$R18Object = Import-Csv -Path $R18File | Select-Object -ExpandProperty Name -Unique ThumbURL
-<#
-$R18ScrapeObject = @()
-for ($x = 0; $x -lt $R18Object.Length; $x++) {
-    if ($R18Object.Name[$x] -match $EmbyObject.Name[$x]) {
-        $R18ScrapeObject += New-Object -TypeName psoboject -Property @{
-            Name = $R18ScrapeObject[$x]
-            EmbyId = $Emby
+    else {
+        $Index = [array]::indexof(($R18ActorObject.Name).ToLower(), $ActorNames[$x])
+        #Write-Host ""$EmbyActorObject[$x].Name" is index $Index"
+        $ActorObject += New-Object -TypeName psobject -Property @{
+            Name = $EmbyActorObject[$x].Name
+            EmbyId = $EmbyActorObject[$x].EmbyId
+            ThumbUrl = $R18ActorObject[$Index].ThumbUrl
+            PrimaryUrl = $R18ActorObject[$Index].ThumbUrl
         }
     }
 }
 
-
-$ActorObject = @()
-
-
-$ActorObject = @()
-foreach ($FileObj in $FileObject) {
-    foreach ($ThumbObj in $CleanThumbObject) {
-        if ($ThumbObj.Name -like $FileObj.Name) {
-            # Write matches
-            $ActorObject += New-Object -TypeName psobject -Property @{
-                Name = $ThumbObj.Name
-                EmbyID = $FileObj.EmbyID
-                ThumbURL = $ThumbObj.ThumbURL
-                PrimaryURL = $ThumbObj.ThumbURL
-            }
-        }
-    }
-    # Write non-matches
-    $ActorObject += New-Object -TypeName psobject -Property @{
-        Name = $FileObj.Name
-        EmbyID = $FileObj.EmbyID
-    }
-}
-
-
-#$ActorObject = New-ActorObject -Name  #>
+$ActorObject | Export-Csv -Path $ActorExportPath
