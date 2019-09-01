0fae10c34b7ea42c25e6565a3d1a1fca2258d904
jvl <jli141928@gmail.com>
2019-08-31 18:59:57 -0700

    Enable push to csv

--- emby_thumbs/Set-EmbyActorThumbs.ps1
+++ emby_thumbs/Set-EmbyActorThumbs.ps1
@@ -1,4 +1,4 @@
-function Set-ActorThumbs {
+function Add-ActorThumbs {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
@@ -6,23 +6,38 @@ function Set-ActorThumbs {
         [Parameter(Mandatory=$true)]
         [int]$ActorId,
         [Parameter(Mandatory=$true)]
-        [string]$ThumbUrl,
+        [string]$ImageUrl,
         [Parameter(Mandatory=$true)]
-        [string]$PrimaryUrl,
+        [string]$ImageType,
         [Parameter(Mandatory=$true)]
         [string]$ApiKey
     )
+
     # Set actor thumbnail
-    Invoke-RestMethod -Method Post -Uri "$ServerUri/emby/Items/$ActorId/RemoteImages/Download?Type=Thumb&ImageUrl=$ThumbURL&api_key=$ApiKey" -Verbose
-    # Set actor primary image
-    Invoke-RestMethod -Method Post -Uri "$ServerUri/emby/Items/$ActorId/RemoteImages/Download?Type=Primary&ImageUrl=$PrimaryURL&api_key=$ApiKey" -Verbose
+    Invoke-RestMethod -Method Post -Uri "$ServerUri/emby/Items/$ActorId/RemoteImages/Download?Type=$ImageType&ImageUrl=$ImageUrl&api_key=$ApiKey" -Verbose
+}
+
+function Remove-ActorThumbs {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ServerUri,
+        [Parameter(Mandatory=$true)]
+        [int]$ActorId,
+        [Parameter(Mandatory=$true)]
+        [string]$ImageType,
+        [Parameter(Mandatory=$true)]
+        [string]$ApiKey
+    )
+
+    Invoke-RestMethod -Method Delete -Uri "$ServerUri/emby/Items/$ActorId/Images/Download?Type=$ImageType&api_key=$ApiKey" -Verbose
 }
 
 # Remove progress bar to speed up REST requests
 $ProgressPreference = 'SilentlyContinue'
 
-$SettingsPath = Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath (Join-Path -Path '..' -ChildPath 'settings_sort_jav.ini'))
 # Check settings file for config
+$SettingsPath = Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath (Join-Path -Path '..' -ChildPath 'settings_sort_jav.ini'))
 $EmbyServerUri = ((Get-Content $SettingsPath) -match '^emby-server-uri').Split('=')[1]
 $EmbyApiKey = ((Get-Content $SettingsPath) -match '^emby-api-key').Split('=')[1]
 $ActorImportPath = ((Get-Content $SettingsPath) -match '^actor-csv-export-path').Split('=')[1]
@@ -30,12 +45,65 @@ $ActorDbPath = ((Get-Content $SettingsPath) -match '^actor-csv-database-path').S
 
 $ActorObject = Import-Csv -Path $ActorImportPath
 
+# Check if db file specified in 'actor-csv-database-path' exists, create if not exists
 if (!(Test-Path $ActorDbPath)) {
     Write-Host "Database file not found. Creating..."
     New-Item -ItemType File -Path $ActorDbPath
 }
 
-foreach ($Actor in $ActorObject) {
-    # Set-ActorThumbs -ServerUri $EmbyServerUri -ActorId $ActorObject.EmbyID -ThumbUrl $ActorObject.ThumbURL -PrimaryUrl $Object.ActorPrimaryURL -ApiKey $EmbyApiKey
-    $Actor | Export-Csv -Path $ActorDbPath -Append -NoClobber
+else {
+    $ActorDbObject = Import-Csv -Path $ActorDbPath
+
+    for ($x = 0; $x -lt $ActorObject.Length; $x++) {
+        #$Index = [array]::indexof(($ActorDbObject.Name).ToLower(), $ActorNames[$x])
+        if ($ActorObject[$x].Name -notin $ActorDbObject.Name -and $ActorObject[$x].EmbyId -notin $ActorDbObject.EmbyId) {
+            Write-Host "Adding images to $ActorObject[$x]"
+            # POST thumb to Emby
+            Set-ActorThumbs -ServerUri $EmbyServerUri -ActorId $ActorObject.EmbyID -ImageUrl $ActorObject.ThumbURL -ImageType Thumb -ApiKey $EmbyApiKey
+
+            # POST primary to Emby
+            Set-ActorThumbs -ServerUri $EmbyServerUri -ActorId $ActorObject.EmbyID -ImageUrl $ActorObject.PrimaryUrl -ImageType Thumb -ApiKey $EmbyApiKey
+
+            # Write to db file if posted
+            $ActorObject[$x] | Export-Csv -Path $ActorDbPath -Append -NoClobber
+        }
+        
+        else {
+            $Index = [array]::indexof(($ActorDbObject.Name).ToLower(), $ActorNames[$x])
+            Write-Host ""$EmbyActorObject[$x].Name" is index $Index"
+            if ($ActorObject[$x].Name -eq $ActorDbObject[$Index].Name -and $ActorObject[$x].EmbyId -eq $ActorDbObject[$Index].EmbyId) {
+                Write-Host "Match"
+                if ($ActorObject[$x].ThumbUrl -notlike $ActorDbObject[$Index].ThumbUrl) {
+                    if ($null -eq $ActorObject[$x].ThumbUrl) {
+                        Write-Host "Removing thumb for $ActorObject[$x]"
+                        Remove-ActorThumbs -ServerUri $EmbyServerUri -ActorId $ActorObject[$x].EmbyId -ImageType Thumb -ApiKey $EmbyApiKey
+                        $ActorObject[$x] | Export-Csv -Path $ActorDbPath -Append -NoClobber
+                    }
+
+                    else {
+                        Write-Host "Adding thumb for $ActorObject[$x]"
+                        Set-ActorThumbs -ServerUri $EmbyServerUri -ActorId $ActorObject[$x].EmbyID -ImageUrl $ActorObject[$x].ThumbURL -ImageType Thumb -ApiKey $EmbyApiKey
+                        $ActorObject[$x] | Export-Csv -Path $ActorDbPath -Append -NoClobber
+                    }
+                }
+
+                if ($ActorObject[$x].PrimaryUrl -notlike $ActorDbObject[$Index].PrimaryUrl) {
+                    if ($null -eq $ActorObject[$x].PrimaryUrl) {
+                        Write-Host "Removing primary for $ActorObject[$x]"
+                        Remove-ActorThumbs -ServerUri $EmbyServerUri -ActorId $ActorObject[$x].EmbyId -ImageType Primary -ApiKey $EmbyApiKey
+                        $ActorObject[$x] | Export-Csv -Path $ActorDbPath -Append -NoClobber
+                    }
+
+                    else {
+                        Write-Host "Adding primary for $ActorObject[$x]"
+                        Set-ActorThumbs -ServerUri $EmbyServerUri -ActorId $ActorObject[$x].EmbyId -ImageUrl $ActorObject[$x].PrimaryUrl -ImageType Primary -ApiKey $EmbyApiKey
+                        $ActorObject[$x] | Export-Csv -Path $ActorDbPath -Append -NoClobber
+                    }
+                }
+            }
+            else {
+                $ActorObject[$x] | Export-Csv -Path $ActorDbPath -Append -NoClobber
+            }
+        }
+    }
 }
