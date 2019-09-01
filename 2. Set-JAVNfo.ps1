0cc3749002edc6caf560332c5063db90f0cbcaf5
jvl <jli141928@gmail.com>
2019-09-01 02:58:51 -0700

    Increase actor regex scope and adjust formatting

--- 2. Set-JAVNfo.ps1
+++ 2. Set-JAVNfo.ps1
@@ -10,9 +10,9 @@ function Set-JAVNfo {
 
     function Show-FileChanges {
         # Display file changes to host
-        $Table = @{Expression={$_.Index}; Label="#"; Width=2},
-                 @{Expression={$_.Name}; Label="Name"; Width=25},
-                 @{Expression={$_.Path}; Label="Directory"}
+        $Table = @{Expression = { $_.Index }; Label = "#"; Width = 2 },
+        @{Expression = { $_.Name }; Label = "Name"; Width = 25 },
+        @{Expression = { $_.Path }; Label = "Directory" }
         $FileObject | Sort-Object Index | Format-Table -Property $Table | Out-Host
     }
 
@@ -25,6 +25,7 @@ function Set-JAVNfo {
 
     # Write txt metadata file paths to $HtmlMetadata
     $HtmlMetadata = Get-ChildItem -LiteralPath $FilePath -Recurse | Where-Object { $_.Name -match '[a-zA-Z]{1,8}-[0-9]{1,8}(.*.txt)' -or $_.Name -match 't28(.*).txt' } | Select-Object Name, BaseName, FullName, Directory
+
     if ($null -eq $HtmlMetadata) {
         Write-Warning 'No metadata files found! Exiting...'
         pause
@@ -34,17 +35,19 @@ function Set-JAVNfo {
         # Create table to show files being written
         $Index = 1
         $FileObject = @()
+
         foreach ($File in $HtmlMetadata) {
             $FileObject += New-Object -TypeName psobject -Property @{
                 Index = $Index
-                Name = $File.BaseName
-                Path = $File.Directory
+                Name  = $File.BaseName
+                Path  = $File.Directory
             }
             $Index++
         }
 
         # Default prompt yes
         $Input = 'y'
+
         if ($Prompt) {
             Write-Output "Metadata to be written:"
             Show-FileChanges
@@ -58,6 +61,7 @@ function Set-JAVNfo {
             # Write each nfo file
             $Count = 1
             $Total = $HtmlMetadata.Count
+
             foreach ($MetadataFile in $HtmlMetadata) {
                 # Read html txt
                 $FileLocation = $MetadataFile.FullName
@@ -68,8 +72,8 @@ function Set-JAVNfo {
 
                 # Get metadata information from txt file
                 $Title = $HtmlContent -match '<title>(.*) - JAVLibrary<\/title>'
-                $TitleFixed = (($Title -replace '<title>', '') -replace  '- JAVLibrary</title>', '').Trim()
-                $ReleaseDate = ($HtmlContent -match '<td class="text">\d{4}-\d{2}-\d{2}<\/td>').Split(('<td class="text">','</td>'), 'None')[1]
+                $TitleFixed = (($Title -replace '<title>', '') -replace '- JAVLibrary</title>', '').Trim()
+                $ReleaseDate = ($HtmlContent -match '<td class="text">\d{4}-\d{2}-\d{2}<\/td>').Split(('<td class="text">', '</td>'), 'None')[1]
                 $ReleaseYear = ($ReleaseDate.Split('-'))[0]
                 $Studio = (($HtmlContent -match '<a href="vl_maker\.php\?m=[\w\d]{1,10}" rel="tag">(.*)<\/a>')).Split(('rel="tag">', '</a> &nbsp'), 'None')[1]
                 $Genres = (($HtmlContent -match 'rel="category tag">(.*)<\/a><\/span><\/td>') -Split 'rel="category tag">')
@@ -77,45 +81,53 @@ function Set-JAVNfo {
                 # Write metadata to file
                 Set-Content -LiteralPath $NfoPath -Value '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' -Force
                 Add-Content -LiteralPath $NfoPath -Value '<movie>'
+
                 if ($AddTitle -like 'true') {
                     Add-Content -LiteralPath $NfoPath -Value "    <title>$TitleFixed</title>"
                 }
+
                 Add-Content -LiteralPath $NfoPath -Value "    <year>$ReleaseYear</year>"
                 Add-Content -LiteralPath $NfoPath -Value "    <releasedate>$ReleaseDate</releasedate>"
                 Add-Content -LiteralPath $NfoPath -Value "    <studio>$Studio</studio>"
+
                 if ($AddGenres -like 'true') {
-                    foreach ($Genre in $Genres[1..($Genres.Length-1)]) {
+                    foreach ($Genre in $Genres[1..($Genres.Length - 1)]) {
                         $GenreString = (($Genre.Split('<'))[0]).Trim()
                         Add-Content -LiteralPath $NfoPath -Value "    <genre>$GenreString</genre>"
                     }
                 }
+
                 if ($AddTags -like 'true') {
-                    foreach ($Genre in $Genres[1..($Genres.Length-1)]) {
+                    foreach ($Genre in $Genres[1..($Genres.Length - 1)]) {
                         $GenreString = (($Genre.Split('<'))[0]).Trim()
                         Add-Content -LiteralPath $NfoPath -Value "    <tag>$GenreString</tag>"
                     }
                 }
+
                 # Add actress metadata
                 $ActorSplitString = '<span class="star">'
                 $ActorSplitHtml = $HtmlContent -split $ActorSplitString
                 $Actors = @()
+
                 foreach ($Section in $ActorSplitHtml) {
                     $FullName = (($Section -split "rel=`"tag`">")[1] -split "<\/a><\/span>")[0]
                     if ($FullName -ne '') {
-                        if ($FullName.Length -lt 25) {
+                        if ($FullName.Length -lt 40) {
                             $Actors += $FullName
                         }
                     }
                 }
+
                 foreach ($Actor in $Actors) {
-                            $Content = @(
-                            "    <actor>"
-                            "        <name>$Actor</name>"
-                            "        <role>Actor</role>"
-                            "    </actor>"
+                    $Content = @(
+                        "    <actor>"
+                        "        <name>$Actor</name>"
+                        "        <role>Actor</role>"
+                        "    </actor>"
                     )
                     Add-Content -LiteralPath $NfoPath -Value $Content
                 }
+
                 # End file
                 Add-Content -LiteralPath $NfoPath -Value '</movie>'
                 Write-Output "($Count of $Total) $FileName .nfo processed..."
@@ -123,10 +135,13 @@ function Set-JAVNfo {
                 if ($KeepMetadataTxt -eq 'false') {
                     Remove-Item -LiteralPath $MetadataFile.FullName
                 }
+
                 $Count++
             }
+
             pause
         }
+
         else {
             Write-Warning 'Cancelled by user input. Exiting...'
             pause
