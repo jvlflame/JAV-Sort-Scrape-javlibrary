1e2a32e8a24a87ce86e12b6b6a12722e9d095685
jvl <jli141928@gmail.com>
2019-09-05 00:15:35 -0700

    Scrape R18 title

--- 2. Set-JAVNfo.ps1
+++ 2. Set-JAVNfo.ps1
@@ -16,6 +16,9 @@ function Set-JAVNfo {
         $FileObject | Sort-Object Index | Format-Table -Property $Table | Out-Host
     }
 
+    # Remove progress bar to speed up REST requests
+    $ProgressPreference = 'SilentlyContinue'
+
     # Options from settings_sort_jav.ini
     $SettingsPath = Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath 'settings_sort_jav.ini')
     $KeepMetadataTxt = ((Get-Content $SettingsPath) -match '^keep-metadata-txt').Split('=')[1]
@@ -24,6 +27,7 @@ function Set-JAVNfo {
     $AddTitle = ((Get-Content $SettingsPath) -match '^include-video-title').Split('=')[1]
     $PartDelimiter = ((Get-Content $SettingsPath) -match '^delimiter-between-multiple-videos').Split('=')[1]
     $NameSetting = ((Get-Content $SettingsPath) -match '^actress-before-video-number').Split('=')[1]
+    $R18TitleCheck = ((Get-Content $SettingsPath) -match '^prefer-r18-title').Split('=')[1]
 
     Write-Host "Metadata to be written:"
     # Write txt metadata file paths to $HtmlMetadata
@@ -65,12 +69,21 @@ function Set-JAVNfo {
                 $HtmlContent = Get-Content -LiteralPath $FileLocation -Encoding UTF8
                 $FileName = $MetadataFile.BaseName
                 $NfoName = $MetadataFile.BaseName + '.nfo'
+                $VideoId = ($FileName -split "$PartDelimiter")[0]
                 $NfoPath = Join-Path -Path $MetadataFile.Directory -ChildPath $NfoName
-                # Get video title name from html with regex
-                $Title = $HtmlContent -match '<title>(.*) - JAVLibrary<\/title>'
-                # Remove broken HTML causing title not to write correctly
-                $TitleFixHTML = ($Title -replace '&quot;', '') -replace '#39;s', ''
-                $TitleFixed = ((($TitleFixHTML -replace '<title>', '') -replace '- JAVLibrary</title>', '').Trim()) -replace ' [\W]', ''
+                if ($R18Title -like 'false') {
+                    # Get video title name from html with regex
+                    $Title = $HtmlContent -match '<title>(.*) - JAVLibrary<\/title>'
+                    # Remove broken HTML causing title not to write correctly
+                    $TitleFixHTML = ($Title -replace '&quot;', '') -replace '#39;s', ''
+                    $TitleFixed = ((($TitleFixHTML -replace '<title>', '') -replace '- JAVLibrary</title>', '').Trim()) -replace ' [\W]', ''
+                }
+                else {
+                    $R18Search = Invoke-WebRequest "https://www.r18.com/common/search/searchword=$VideoId/"
+                    $R18Url = (($R18Search.Links | Where-Object {$_.href -like "*/videos/vod/movies/detail/-/id=*"}).href)[0]
+                    $R18Title = (($R18Search.Links | Where-Object {$_.href -like "*/videos/vod/movies/detail/-/id=*"}).innertext -split "~\d{1,3}.\d{1,2}").Trim()[0]
+                }
+
                 # Check if the video has multiple parts
                 # If it does, write the part number to a variable
                 if ($NameSetting -like 'true') {
