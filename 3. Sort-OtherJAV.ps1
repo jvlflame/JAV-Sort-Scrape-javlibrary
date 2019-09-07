f5148d426c370c7412c42b88627156294effee4c
jvl <jli141928@gmail.com>
2019-09-07 01:55:34 -0700

    Set language to jaat and unique tags

--- 3. Sort-OtherJAV.ps1
+++ 3. Sort-OtherJAV.ps1
@@ -4,7 +4,7 @@ $GoogleSearch = Invoke-WebRequest -Uri "http://www.google.com/search?q=site:7mmt
 $7mmLink = (((((($GoogleSearch.Links.href -match '7mmtv.tv/../amateurjav_content')) -replace '7mmtv.tv/..', '7mmtv.tv/en') -replace '\/url\?q=', '') -split "&amp;")[0])
 Write-Host $7mmLink
 if ($7mmLink -notmatch $VideoId) {
-    $7mmLink = (((((($GoogleSearch.Links.href -match '7mmtv.tv/../uncensored_content')) -replace '7mmtv.tv/..', '7mmtv.tv/en') -replace '\/url\?q=', '') -split "&amp;")[0])
+    $7mmLink = (((((($GoogleSearch.Links.href -match '7mmtv.tv/../uncensored_content')) -replace '7mmtv.tv/..', '7mmtv.tv/jaat') -replace '\/url\?q=', '') -split "&amp;")[0])
     Write-Host $7mmLink
     if ($7mmLink -notmatch $VideoId -or $null -like $7mmLink -or $7mmLink -like '') {
         "$VideoId not found on 7mmtv. Skipping..."
@@ -16,4 +16,4 @@ $7mmScrape = Invoke-WebRequest -Uri $7mmLink
 
 $ScrapedTitle = ((($7mmScrape.ParsedHtml.title -split '\]')[1]) -split ' - ')[0]
 $Title = "$VideoId $ScrapedTItle"
-$Tags = $7mmScrape.Links | Where-Object { $_.outerHTML -match '_category\/' }
+$Tags = ($7mmScrape.Links | Where-Object { $_.outerHTML -match '_category\/' }).InnerHTML | Select-Object -Unique
