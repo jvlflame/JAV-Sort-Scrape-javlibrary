5ca1a1c6b8abbc33720f5f887dc43af09cc7644f
jvl <jli141928@gmail.com>
2019-09-07 13:40:50 -0700

    Fix settings dependency

--- 2. Set-JAVNfo.ps1
+++ 2. Set-JAVNfo.ps1
@@ -97,10 +97,12 @@ function Set-JAVNfo {
                 # Remove broken HTML causing title not to write correctly
                 $TitleFixHTML = ($Title -replace '&quot;', '') -replace '#39;s', ''
                 $TitleFixed = ((($TitleFixHTML -replace '<title>', '') -replace '- JAVLibrary</title>', '').Trim())
-                if ($R18TitleCheck -like 'true') {
+                if ($R18TitleCheck -like 'true' -or $R18MetadataCheck -like 'true') {
                     # Perform a search on R18.com for the video ID
                     $R18Search = Invoke-WebRequest "https://www.r18.com/common/search/searchword=$VideoId/"
                     $R18Url = (($R18Search.Links | Where-Object {$_.href -like "*/videos/vod/movies/detail/-/id=*"}).href)
+                }
+                if ($R18TitleCheck -like 'true') {
                     if ($R18Search.Content -match "data-product-page-url=`"https://www.r18.com/videos/vod/movies/detail") {
                         $R18Title = (((($R18Search.Content -split "data-title=`"")[1] -split "data-title=`"")[0] -split "data-description")[0].Trim()) -replace ".$"
                     }
