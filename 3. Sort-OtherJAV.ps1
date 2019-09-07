ee392ddcdc7d155d0f83568736386eb12a0ce6fa
jvl <jli141928@gmail.com>
2019-09-07 01:55:15 -0700

    Update progress on 7mm scrape

--- 3. Sort-OtherJAV.ps1
+++ 3. Sort-OtherJAV.ps1
@@ -1,20 +1,19 @@
 
-$VideoId = "336DTT-036"
+$VideoId = "sg032"
 $GoogleSearch = Invoke-WebRequest -Uri "http://www.google.com/search?q=site:7mmtv.tv+$VideoId"
-$7mmLink = ((((($GoogleSearch.Links.href -match "https://7mmtv.tv/zh/amateurjav_content")) -replace 'tv/zh', 'tv/en') -replace '\/url\?q=', '') -split "&amp;")[0]
-if ($7mmLink -match $VideoId) {
-    $7mmLink = ((((($GoogleSearch.Links.href -match "https://7mmtv.tv/zh/uncensored_content")) -replace 'tv/zh', 'tv/en') -replace '\/url\?q=', '') -split "&amp;")[0]
+$7mmLink = (((((($GoogleSearch.Links.href -match '7mmtv.tv/../amateurjav_content')) -replace '7mmtv.tv/..', '7mmtv.tv/en') -replace '\/url\?q=', '') -split "&amp;")[0])
+Write-Host $7mmLink
+if ($7mmLink -notmatch $VideoId) {
+    $7mmLink = (((((($GoogleSearch.Links.href -match '7mmtv.tv/../uncensored_content')) -replace '7mmtv.tv/..', '7mmtv.tv/en') -replace '\/url\?q=', '') -split "&amp;")[0])
+    Write-Host $7mmLink
+    if ($7mmLink -notmatch $VideoId -or $null -like $7mmLink -or $7mmLink -like '') {
+        "$VideoId not found on 7mmtv. Skipping..."
+        return
+    }
 }
-
-else {
-    $7mmLink = ((((($GoogleSearch.Links.href -match "https://7mmtv.tv/zh/uncensored_content")) -replace 'tv/zh', 'tv/en') -replace '\/url\?q=', '') -split "&amp;")[0]
-}
-
-else ($null -like $7mmLink -or $7mmLink -like '') {
-    "$VideoId not found on 7mmtv. Skipping..."
-}
-
 
 $7mmScrape = Invoke-WebRequest -Uri $7mmLink
 
-$Title = ($7mmScrape.ParsedHtml.title -replace '\[','') -replace '\]', '' 
+$ScrapedTitle = ((($7mmScrape.ParsedHtml.title -split '\]')[1]) -split ' - ')[0]
+$Title = "$VideoId $ScrapedTItle"
+$Tags = $7mmScrape.Links | Where-Object { $_.outerHTML -match '_category\/' }
